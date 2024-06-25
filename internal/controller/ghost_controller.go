c55097315f1a1c199dff61a42564668afcd284d7
Shahrooz Aghili <shahrooz33ce@gmail.com>
2024-06-25 20:14:38 +0200

    add marker for core events

--- internal/controller/ghost_controller.go
+++ internal/controller/ghost_controller.go
@@ -53,6 +53,7 @@ const svcNamePrefix = "ghost-service-"
 //+kubebuilder:rbac:groups="",resources=persistentvolumeclaims,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=apps,resources=deployments,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups="",resources=services,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups="",resources=events,verbs=get;list;watch;create;update;patch
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
